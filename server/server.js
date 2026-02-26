1e7caf9d8a8a0401fa2e8faef7bb91b4ac4ff917
Tanuja223 <tanujabuddepu@gmail.com>
2026-02-26 22:33:34 +0530

    fix cors properly

--- server/server.js
+++ server/server.js
@@ -1,41 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const orderRoutes = require("./routes/order.routes");
-const app = express();
 const cron = require("node-cron");
+
+const orderRoutes = require("./routes/order.routes");
+const menuRoutes = require("./routes/menuitems.routes");
+const authRoutes = require("./routes/auth.routes");
 const db = require("./config/db");
-import cors from "cors";
 
-app.use(cors());
-// run every day at midnight
-cron.schedule("0 0 * * *", async () => {
-  try {
-    const result = await db.query(`
-      DELETE FROM orders
-      WHERE status = 'paid'
-      AND paid_at <= NOW() - INTERVAL 7 DAY
-    `);
+const app = express();
 
-    console.log("Deleted old paid orders");
-  } catch (err) {
-    console.error(err);
-  }
-});
-
-
+// ✅ CORS (only once)
 app.use(cors());
 
+// JSON middleware
 app.use(express.json());
 
-const menuRoutes = require("./routes/menuitems.routes");
-
-/* ROUTES */
+// Routes
 app.use("/api/menuitems", menuRoutes);
-
 app.use("/api/orders", orderRoutes);
-
-const authRoutes = require("./routes/auth.routes");
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
